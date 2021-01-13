2879bfb93bb5b3ff92fe760a6d27e111aa5917d6
AzureAD\SambaGueye <samba.gueye@toulouse.miage.fr>
2021-01-13 22:53:19 +0100

    refixer la barre de navigation sur mobile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <h3 style={{textAlign:'center'}}>Les dernières publications</h3>
+      <h3 style={{textAlign:'center', paddingTop:'3em', paddingBottom:'2em'}}>Les dernières publications</h3>
       <div className="tweets">
       <Tweet className='hey' title='Election presidentielles 2024' message='Je vous invite tou à voter pour SONKO parce que on en a marre de Mack Sall, il est la que pour son interet et exécute les règles dictées par la France.' likes='100k' img='https://upload.wikimedia.org/wikipedia/commons/d/d4/Candidat-ousmane-sonko.png'/>
       <Tweet className='hey' title='Corona virus en France' message='Le gouvernement annonce letat durgence Un régime dexception qui nen finit plus. Le gouvernement va présenter au prochain conseil des ministres, le 13 janvier, un projet visant à proroger dun an, jusquau 31 décembre 2021, le régime actuel de létat durgence sanitaire, a indiqué à LCI une source gouvernementale, confirmant une information de Libération.'  likes='100k' img='https://www.wgcdn.net/juritravail/image/tinymec/e096ea704e7e0a6df1bc95746cfbd5ac.jpg' />
